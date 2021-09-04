492824d3e3b65e94365ae151ee0b78cc2cca1776
jcoro <john@coronite.net>
2021-09-04 04:17:43 -0700

    Added README Step-By-Step

--- src/main/java/com/example/cse_412_project/Cse412ProjectApplication.java
+++ src/main/java/com/example/cse_412_project/Cse412ProjectApplication.java
@@ -1,16 +1,7 @@
 package com.example.cse_412_project;
 
-import com.example.cse_412_project.entities.impl.AppUser;
-import com.example.cse_412_project.entities.impl.AppUser;
-import com.example.cse_412_project.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Properties;
-import java.util.stream.Stream;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 
 @SpringBootApplication
 public class Cse412ProjectApplication {
@@ -18,16 +9,5 @@ public class Cse412ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(Cse412ProjectApplication.class, args);
     }
-
-//    @Bean
-//    CommandLineRunner init(UserRepository userRepository) {
-//        return args -> {
-//            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
-//                AppUser user = new AppUser(name, name.toLowerCase() + "@domain.com");
-//                userRepository.save(user);
-//            });
-//            userRepository.findAll().forEach(System.out::println);
-//        };
-//    }
 
 }
