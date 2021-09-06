0364ed6190e76b024c939d8678bb4f7f12330e40
truongquanghai <truongquanghai9@gmail.com>
2021-09-06 15:57:01 -0700

    - Created entity for FoodDescription and a little bit of NutData (Still missing a lot) - Created FoodDescriptionDto (in progress, still need to wait for the other entities to be done) - Added mapperDto in pom.xml - After the other entities are done => work on parsing the file's data into the database

---
+++ src/main/java/com/example/cse_412_project/entities/FoodDescription.java
@@ -0,0 +1,71 @@
+package com.example.cse_412_project.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "FOOD_DES")
+public class FoodDescription {
+
+    @Id
+    @Column(name = "NDB_No", nullable = false)
+    private int ndbNo;
+
+    @Column(name = "FdGrp_Cd")
+    private String fdGrpCd;
+
+    @Column(name = "Long_Desc")
+    private String longDesc;
+
+    @Column(name = "Shrt_Desc")
+    private String shortDesc;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "foodDescription", cascade = CascadeType.DETACH)
+    private List<NutData> nutDataList;
+
+    // todo: Missing Weight
+
+    // todo: Missing fdGroup
+
+    // todo: Missing JournalEntry
+
+    public int getNdbNo() {
+        return ndbNo;
+    }
+
+    public void setNdbNo(int NDB_No) {
+        this.ndbNo = NDB_No;
+    }
+
+    public String getFdGrpCd() {
+        return fdGrpCd;
+    }
+
+    public void setFdGrpCd(String fdGrpCd) {
+        this.fdGrpCd = fdGrpCd;
+    }
+
+    public String getLongDesc() {
+        return longDesc;
+    }
+
+    public void setLongDesc(String longDesc) {
+        this.longDesc = longDesc;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+
+    public List<NutData> getNutDataList() {
+        return nutDataList;
+    }
+
+    public void setNutDataList(List<NutData> nutDataList) {
+        this.nutDataList = nutDataList;
+    }
+}
